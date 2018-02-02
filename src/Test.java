ed868964cd64fa9cdae6bdf17687f467f1c4e5cb
Zheny <zheny89@yandex.ru>
2018-02-03 00:19:48 +0500

    First commit

---
+++ src/Test.java
@@ -0,0 +1,17 @@
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public class Test {
+    public static void main(String args[]) throws Exception {
+        String pat =
+                "^(([01]?\\d\\d?)||(2[0-4]\\d)||(25[0-5]))." +
+                        "([01]?\\d\\d?||2[0-4]\\d||25[0-5])." +
+                        "([01]?\\d\\d?||2[0-4]\\d||25[0-5])." +
+                        "([01]?\\d\\d?||2[0-4]\\d||25[0-5])$";
+        String str = "192.168.1.250";
+
+        Pattern pattern = Pattern.compile(pat);
+        Matcher matcher = pattern.matcher("12.");
+        System.out.println(matcher.matches());
+    }
+}
