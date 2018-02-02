cb3ad8714da45342f537104080882c71b1bd0a77
Zheny <zheny89@yandex.ru>
2018-02-03 02:24:06 +0500

    C

--- src/Test.java
+++ src/Test.java
@@ -9,6 +9,9 @@ public class Test {
                         "([01]?\\d\\d?||2[0-4]\\d||25[0-5])." +
                         "([01]?\\d\\d?||2[0-4]\\d||25[0-5])$";
         String str = "192.168.1.250";
+        String str5 = "192.168.1.250";
+        String str4 = "192.168.1.250";
+        String str3 = "192.168.1.250";
 
         Pattern pattern = Pattern.compile(pat);
         Matcher matcher = pattern.matcher("12.");
