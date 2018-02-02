0f4ed4ed58cbdb3f416f32020f46b7cde793371c
Zheny <zheny89@yandex.ru>
2018-02-03 00:48:43 +0500

    Third Commit

--- src/Test.java
+++ src/Test.java
@@ -10,6 +10,7 @@ public class Test {
                         "([01]?\\d\\d?||2[0-4]\\d||25[0-5])$";
         String str = "192.168.1.250";
         String str2 = "192.168.1.250";
+        String str3 = "192.168.1.250";
 
         Pattern pattern = Pattern.compile(pat);
         Matcher matcher = pattern.matcher("12.");
