265f1187d7c18f24ef12097763c3ee868bc61de3
Zheny <zheny89@yandex.ru>
2018-02-03 02:21:29 +0500

    Commit

--- src/Test.java
+++ src/Test.java
@@ -9,11 +9,6 @@ public class Test {
                         "([01]?\\d\\d?||2[0-4]\\d||25[0-5])." +
                         "([01]?\\d\\d?||2[0-4]\\d||25[0-5])$";
         String str = "192.168.1.250";
-        String str2 = "192.168.1.250";
-        String str3 = "192.168.1.250";
-        String str4 = "192.168.1.250";
-        String str5 = "192.168.1.250";
-        String str6 = "192.168.1.250";
 
         Pattern pattern = Pattern.compile(pat);
         Matcher matcher = pattern.matcher("12.");
